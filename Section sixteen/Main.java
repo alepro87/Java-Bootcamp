1f98612cd4b0c7d9ed35fe4121edb9be2b4ede5b
alepro87 <djleshik@gmail.com>
2023-01-02 23:00:34 +0200

    New files + updates

--- Section sixteen/Main.java
+++ Section sixteen/Main.java
@@ -8,10 +8,6 @@ public class Main {
         // nissan.price = 10000;
         // nissan.year = 2020;
         // nissan.color = "Green";
-        Car nissan2 = new Car(nissan);
-        nissan2.setColor("Yellow");
-        nissan2.setPrice(9000);
-        nissan2.setYear(2019);
 
         Car dodge = new Car("Dodge", 11000, 2019, "Blue");
         // dodge.make = "Dodge";
@@ -19,9 +15,17 @@ public class Main {
         // dodge.year = 2019;
         // dodge.color = "Blue";
 
-        System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() + 
-        ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
-        System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() + 
-        ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
+        //Creating a car using copy constructor and updating some fields.
+        Car nissan2 = new Car(nissan);
+        nissan2.setColor("Yellow");
+        nissan2.setPrice(9000);
+        nissan2.setYear(2019);
+
+        // System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() + 
+        // ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
+        // System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() + 
+        // ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
+
+        nissan2.drive();
     }
 }
